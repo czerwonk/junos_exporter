bd5a9c9913805d664e67d179c66f819ea779df13
Daniel Czerwonk <daniel@dan-nrw.de>
2017-06-18 21:40:00 +0200

    refactoring to add non device metrics

--- junos_collector.go
+++ junos_collector.go
@@ -15,8 +15,8 @@ import (
 type ValueConverter func(uint) float64
 
 const (
-	numberOfLabels = 2
-	prefix         = "junos_"
+	numberOfInterfaceLabels = 2
+	prefix                  = "junos_"
 )
 
 var (
@@ -47,10 +47,10 @@ type JunosCollector struct {
 }
 
 type scope struct {
-	labelValues map[string][]string
-	snmp        *gosnmp.GoSNMP
-	ch          chan<- prometheus.Metric
-	err         error
+	interfaceLabels map[string][]string
+	snmp            *gosnmp.GoSNMP
+	ch              chan<- prometheus.Metric
+	err             error
 }
 
 func NewJunosCollector(targets []string, community string) *JunosCollector {
@@ -81,7 +81,7 @@ func (c *JunosCollector) Collect(ch chan<- prometheus.Metric) {
 func (c *JunosCollector) collectForTarget(target string, ch chan<- prometheus.Metric, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	s := &scope{labelValues: make(map[string][]string), snmp: &gosnmp.GoSNMP{}, ch: ch}
+	s := &scope{interfaceLabels: make(map[string][]string), snmp: &gosnmp.GoSNMP{}, ch: ch}
 	s.snmp.Port = 161
 	s.snmp.Timeout = time.Duration(2) * time.Second
 	s.snmp.Target = target
@@ -114,18 +114,18 @@ func (c *JunosCollector) collectMetrics(s *scope) {
 
 	defer s.snmp.Conn.Close()
 
-	c.fetchLabelFromOid(".1.3.6.1.2.1.31.1.1.1.1", 0, s)
-	c.fetchLabelFromOid(".1.3.6.1.2.1.31.1.1.1.18", 1, s)
+	c.fetchInterfaceLabelFromOid(".1.3.6.1.2.1.31.1.1.1.1", 0, s)
+	c.fetchInterfaceLabelFromOid(".1.3.6.1.2.1.31.1.1.1.18", 1, s)
 
-	c.fetchMetricFromOid(".1.3.6.1.2.1.2.2.1.10", receiveBytesDesc, bitsToBytes, s)
-	c.fetchMetricFromOid(".1.3.6.1.2.1.2.2.1.16", transmitBytesDesc, bitsToBytes, s)
-	c.fetchMetricFromOid(".1.3.6.1.2.1.2.2.1.13", receiveDropsDesc, noConvert, s)
-	c.fetchMetricFromOid(".1.3.6.1.2.1.2.2.1.14", receiveErrorsDesc, noConvert, s)
-	c.fetchMetricFromOid(".1.3.6.1.2.1.2.2.1.19", transmitDropsDesc, noConvert, s)
-	c.fetchMetricFromOid(".1.3.6.1.2.1.2.2.1.20", transmitErrorsDesc, noConvert, s)
+	c.fetchInterfaceMetricFromOid(".1.3.6.1.2.1.2.2.1.10", receiveBytesDesc, bitsToBytes, s)
+	c.fetchInterfaceMetricFromOid(".1.3.6.1.2.1.2.2.1.16", transmitBytesDesc, bitsToBytes, s)
+	c.fetchInterfaceMetricFromOid(".1.3.6.1.2.1.2.2.1.13", receiveDropsDesc, noConvert, s)
+	c.fetchInterfaceMetricFromOid(".1.3.6.1.2.1.2.2.1.14", receiveErrorsDesc, noConvert, s)
+	c.fetchInterfaceMetricFromOid(".1.3.6.1.2.1.2.2.1.19", transmitDropsDesc, noConvert, s)
+	c.fetchInterfaceMetricFromOid(".1.3.6.1.2.1.2.2.1.20", transmitErrorsDesc, noConvert, s)
 }
 
-func (c *JunosCollector) fetchLabelFromOid(oid string, index int, s *scope) {
+func (c *JunosCollector) fetchInterfaceLabelFromOid(oid string, index int, s *scope) {
 	err := s.snmp.Walk(oid, func(pdu gosnmp.SnmpPDU) error {
 		c.handlePduAsLabel(index, pdu, s)
 		return nil
@@ -139,10 +139,10 @@ func (c *JunosCollector) fetchLabelFromOid(oid string, index int, s *scope) {
 func (c *JunosCollector) handlePduAsLabel(index int, p gosnmp.SnmpPDU, s *scope) {
 	id := c.getId(p.Name)
 
-	l, found := s.labelValues[id]
+	l, found := s.interfaceLabels[id]
 	if !found {
-		l = make([]string, numberOfLabels)
-		s.labelValues[id] = l
+		l = make([]string, numberOfInterfaceLabels)
+		s.interfaceLabels[id] = l
 	}
 
 	b := p.Value.([]byte)
@@ -154,7 +154,7 @@ func (c *JunosCollector) getId(oid string) string {
 	return t[len(t)-1]
 }
 
-func (c *JunosCollector) fetchMetricFromOid(oid string, desc *prometheus.Desc, converter ValueConverter, s *scope) {
+func (c *JunosCollector) fetchInterfaceMetricFromOid(oid string, desc *prometheus.Desc, converter ValueConverter, s *scope) {
 	err := s.snmp.Walk(oid, func(pdu gosnmp.SnmpPDU) error {
 		return c.handlePduAsMetric(desc, pdu, converter, s)
 	})
@@ -167,7 +167,7 @@ func (c *JunosCollector) fetchMetricFromOid(oid string, desc *prometheus.Desc, c
 func (c *JunosCollector) handlePduAsMetric(desc *prometheus.Desc, pdu gosnmp.SnmpPDU, converter ValueConverter, s *scope) error {
 	id := c.getId(pdu.Name)
 	v := pdu.Value.(uint)
-	l := append(s.labelValues[id], s.snmp.Target)
+	l := append(s.interfaceLabels[id], s.snmp.Target)
 	m, err := prometheus.NewConstMetric(desc, prometheus.GaugeValue, converter(v), l...)
 
 	if err != nil {

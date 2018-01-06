7e2a7b403500d06cdbcafa0c08eb9bad219c6d81
Daniel Czerwonk <daniel@dan-nrw.de>
2018-01-06 22:41:17 +0100

    goimports

--- rpc/route_prc.go
+++ rpc/route_prc.go
@@ -7,15 +7,15 @@ type RouteRpc struct {
 }
 
 type RouteTable struct {
-	Name string `xml:"table-name"`
-	MaxRoutes int64 `xml:"prefix-max"`
-	TotalRoutes int64 `xml:"total-route-count"`
-	ActiveRoutes int64 `xml:"active-route-count"`
-	Protocols []RouteTableProtocol `xml:"protocols"`
+	Name         string               `xml:"table-name"`
+	MaxRoutes    int64                `xml:"prefix-max"`
+	TotalRoutes  int64                `xml:"total-route-count"`
+	ActiveRoutes int64                `xml:"active-route-count"`
+	Protocols    []RouteTableProtocol `xml:"protocols"`
 }
 
 type RouteTableProtocol struct {
-	Name string `xml:"protocol-name"`
-	Routes int64 `xml:"protocol-route-count"`
-	ActiveRoutes int64 `xml:"active-route-count"`
+	Name         string `xml:"protocol-name"`
+	Routes       int64  `xml:"protocol-route-count"`
+	ActiveRoutes int64  `xml:"active-route-count"`
 }

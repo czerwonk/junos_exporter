8a60e9a5c641be4dc16d99abff77c1f0221aabcc
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2019-01-29 11:00:09 +0100

    Changed type of interface counters to uint64
    resolves #25

--- interfaces/rpc.go
+++ interfaces/rpc.go
@@ -15,12 +15,12 @@ type PhyInterface struct {
 	Stats             TrafficStat    `xml:"traffic-statistics"`
 	LogicalInterfaces []LogInterface `xml:"logical-interface"`
 	InputErrors       struct {
-		Drops  int64 `xml:"input-drops"`
-		Errors int64 `xml:"input-errors"`
+		Drops  uint64 `xml:"input-drops"`
+		Errors uint64 `xml:"input-errors"`
 	} `xml:"input-error-list"`
 	OutputErrors struct {
-		Drops  int64 `xml:"output-drops"`
-		Errors int64 `xml:"output-errors"`
+		Drops  uint64 `xml:"output-drops"`
+		Errors uint64 `xml:"output-errors"`
 	} `xml:"output-error-list"`
 }
 
@@ -31,16 +31,16 @@ type LogInterface struct {
 }
 
 type TrafficStat struct {
-	InputBytes    int64    `xml:"input-bytes"`
-	InputPackets  int64    `xml:"input-packets"`
-	OutputBytes   int64    `xml:"output-bytes"`
-	OutputPackets int64    `xml:"output-packets"`
+	InputBytes    uint64    `xml:"input-bytes"`
+	InputPackets  uint64    `xml:"input-packets"`
+	OutputBytes   uint64    `xml:"output-bytes"`
+	OutputPackets uint64    `xml:"output-packets"`
 	IPv6Traffic   IPv6Stat `xml:"ipv6-transit-statistics"`
 }
 
 type IPv6Stat struct {
-	InputBytes    int64 `xml:"input-bytes"`
-	InputPackets  int64 `xml:"input-packets"`
-	OutputBytes   int64 `xml:"output-bytes"`
-	OutputPackets int64 `xml:"output-packets"`
+	InputBytes    uint64 `xml:"input-bytes"`
+	InputPackets  uint64 `xml:"input-packets"`
+	OutputBytes   uint64 `xml:"output-bytes"`
+	OutputPackets uint64 `xml:"output-packets"`
 }

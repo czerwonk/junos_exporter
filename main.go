5b3680e3a25d61e05e8b60a3e9fcb67ffff08d8c
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2019-05-16 12:22:04 +0200

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 	"github.com/prometheus/common/log"
 )
 
-const version string = "0.9.0"
+const version string = "0.9.1"
 
 var (
 	showVersion                 = flag.Bool("version", false, "Print version information.")

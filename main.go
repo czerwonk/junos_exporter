3e59a5ba34ce0e352767cb79b94f8e5665049d45
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2021-06-09 08:55:28 +0200

    bump version

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 	"github.com/prometheus/common/log"
 )
 
-const version string = "0.9.9"
+const version string = "0.9.10"
 
 var (
 	showVersion                 = flag.Bool("version", false, "Print version information.")

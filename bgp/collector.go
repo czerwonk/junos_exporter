39a9467d8bf263b9e43d4298f1ea8d882280b89d
Gaste8 <jakob.gastinger@gmail.com>
2022-07-20 11:33:05 +0200

    Neteng-1334 add prefix limit (#3)
    * update gitignore
    
    * added prefix-limit(prefix-count) to be exported for each peer
    
    * change variable name for prefix limit (prefix_limit_count)
    
    Co-authored-by: Jakob Gastinger <jgastinger@anexia-it.com>

--- bgp/collector.go
+++ bgp/collector.go
@@ -20,6 +20,7 @@ var (
 	inputMessagesDesc      *prometheus.Desc
 	outputMessagesDesc     *prometheus.Desc
 	flapsDesc              *prometheus.Desc
+	prefixLimitCountDesc   *prometheus.Desc
 )
 
 func init() {
@@ -28,8 +29,10 @@ func init() {
 	inputMessagesDesc = prometheus.NewDesc(prefix+"messages_input_count", "Number of received messages", l, nil)
 	outputMessagesDesc = prometheus.NewDesc(prefix+"messages_output_count", "Number of transmitted messages", l, nil)
 	flapsDesc = prometheus.NewDesc(prefix+"flap_count", "Number of session flaps", l, nil)
+	prefixLimitCountDesc = prometheus.NewDesc(prefix+"prefix_limit_count", "prefix-count variable set in prefix-limit", l, nil)
 
 	l = append(l, "table")
+
 	receivedPrefixesDesc = prometheus.NewDesc(prefix+"prefixes_received_count", "Number of received prefixes", l, nil)
 	acceptedPrefixesDesc = prometheus.NewDesc(prefix+"prefixes_accepted_count", "Number of accepted prefixes", l, nil)
 	rejectedPrefixesDesc = prometheus.NewDesc(prefix+"prefixes_rejected_count", "Number of rejected prefixes", l, nil)
@@ -62,6 +65,7 @@ func (*bgpCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- inputMessagesDesc
 	ch <- outputMessagesDesc
 	ch <- flapsDesc
+	ch <- prefixLimitCountDesc
 }
 
 // Collect collects metrics from JunOS
@@ -109,6 +113,8 @@ func (c *bgpCollector) collectForPeer(p BGPPeer, ch chan<- prometheus.Metric, la
 	ch <- prometheus.MustNewConstMetric(flapsDesc, prometheus.GaugeValue, float64(p.Flaps), l...)
 
 	c.collectRIBForPeer(p, ch, l)
+
+	c.collectPrefixLimitCountForPeer(p, ch, l)
 }
 
 func (*bgpCollector) collectRIBForPeer(p BGPPeer, ch chan<- prometheus.Metric, labelValues []string) {
@@ -120,4 +126,8 @@ func (*bgpCollector) collectRIBForPeer(p BGPPeer, ch chan<- prometheus.Metric, l
 		ch <- prometheus.MustNewConstMetric(activePrefixesDesc, prometheus.GaugeValue, float64(rib.ActivePrefixes), l...)
 		ch <- prometheus.MustNewConstMetric(advertisedPrefixesDesc, prometheus.GaugeValue, float64(rib.AdvertisedPrefixes), l...)
 	}
+}
+
+func (*bgpCollector) collectPrefixLimitCountForPeer(p BGPPeer, ch chan<- prometheus.Metric, labelValues []string) {
+	ch <- prometheus.MustNewConstMetric(prefixLimitCountDesc, prometheus.GaugeValue, float64(p.BGPOI.PrefixLimit.PrefixCount), labelValues...)
 }
--- bgp/rpc.go
+++ bgp/rpc.go
@@ -7,15 +7,16 @@ type BGPRPC struct {
 }
 
 type BGPPeer struct {
-	IP             string `xml:"peer-address"`
-	ASN            string `xml:"peer-as"`
-	State          string `xml:"peer-state"`
-	Group          string `xml:"peer-group"`
-	Description    string `xml:"description"`
-	Flaps          int64  `xml:"flap-count"`
-	InputMessages  int64  `xml:"input-messages"`
-	OutputMessages int64  `xml:"output-messages"`
-	RIBs           []RIB  `xml:"bgp-rib"`
+	IP             string               `xml:"peer-address"`
+	ASN            string               `xml:"peer-as"`
+	State          string               `xml:"peer-state"`
+	Group          string               `xml:"peer-group"`
+	Description    string               `xml:"description"`
+	Flaps          int64                `xml:"flap-count"`
+	InputMessages  int64                `xml:"input-messages"`
+	OutputMessages int64                `xml:"output-messages"`
+	RIBs           []RIB                `xml:"bgp-rib"`
+	BGPOI          BGPOptionInformation `xml:"bgp-option-information"`
 }
 
 type RIB struct {
@@ -25,4 +26,24 @@ type RIB struct {
 	AcceptedPrefixes   int64  `xml:"accepted-prefix-count"`
 	RejectedPrefixes   int64  `xml:"suppressed-prefix-count"`
 	AdvertisedPrefixes int64  `xml:"advertised-prefix-count"`
+}
+
+type BGPOptionInformation struct {
+	ExportPolicy    string      `xml:"export-policy"`
+	ImportPolicy    string      `xml:"import-policy"`
+	AddressFamilies string      `xml:"address-families"`
+	LocalAddress    string      `xml:"local-address"`
+	Holdtime        int64       `xml:"holdtime"`
+	MetricOut       int64       `xml:"metric-out"`
+	Preference      int64       `xml:"preference"`
+	PrefixLimit     PrefixLimit `xml:"prefix-limit"`
+	LocalAs         int64       `xml:"local-as"`
+	LocalSystemAs   int64       `xml:"local-system-as"`
+}
+
+type PrefixLimit struct {
+	NlriType          string `xml:"nlri-type"`
+	PrefixCount       int64  `xml:"prefix-count"`
+	LimitAction       string `xml:"limit-action"`
+	WarningPercentage int64  `xml:"warning-percentage"`
 }

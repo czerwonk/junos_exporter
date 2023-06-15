e2aa1cb3e3e63620e8c90e774c429544c63e0db6
Richard Schwab <richard.schwab@valtech.com>
2023-06-15 21:58:54 +0200

    Fix alarms on multi-RE systems
    This implements handling of multi-RE alarm responses, fixing #216.
    It also introduces a few test cases to ensure multi-RE systems are being
    handled properly in the future.

--- pkg/features/alarm/collector.go
+++ pkg/features/alarm/collector.go
@@ -3,7 +3,9 @@
 package alarm
 
 import (
+	"encoding/xml"
 	"regexp"
+	"strings"
 
 	"github.com/czerwonk/junos_exporter/pkg/collector"
 	"github.com/prometheus/client_golang/prometheus"
@@ -19,7 +21,7 @@ var (
 
 func init() {
 	l := []string{"target"}
-	alarmsYellowCount = prometheus.NewDesc(prefix+"yellow_count", "Number of yollow alarms (not silenced)", l, nil)
+	alarmsYellowCount = prometheus.NewDesc(prefix+"yellow_count", "Number of yellow alarms (not silenced)", l, nil)
 	alarmsRedCount = prometheus.NewDesc(prefix+"red_count", "Number of red alarms (not silenced)", l, nil)
 	l = append(l, "class", "type", "description")
 	alarmDetails = prometheus.NewDesc(prefix+"set", "Alarm active with the details provided in labels", l, nil)
@@ -83,30 +85,34 @@ func (c *alarmCollector) alarmCounter(client collector.Client) (*alarmCounter, *
 
 	messages := make(map[string]interface{})
 	for _, cmd := range cmds {
-		var a = result{}
-		err := client.RunCommandAndParse(cmd, &a)
+		var a = multiEngineResult{}
+		err := client.RunCommandAndParseWithParser(cmd, func(b []byte) error {
+			return parseXML(b, &a)
+		})
 		if err != nil {
 			return nil, nil, err
 		}
 
-		for _, d := range a.Information.Details {
-			if _, found := messages[d.Description]; found {
-				continue
+		for _, engine := range a.Information.RoutingEngines {
+			for _, d := range engine.AlarmInfo.Details {
+				if _, found := messages[d.Description]; found {
+					continue
+				}
+
+				alarms = append(alarms, d)
+
+				if c.shouldFilterAlarm(&d) {
+					continue
+				}
+
+				if d.Class == "Major" {
+					red++
+				} else if d.Class == "Minor" {
+					yellow++
+				}
+
+				messages[d.Description] = nil
 			}
-
-			alarms = append(alarms, d)
-
-			if c.shouldFilterAlarm(&d) {
-				continue
-			}
-
-			if d.Class == "Major" {
-				red++
-			} else if d.Class == "Minor" {
-				yellow++
-			}
-
-			messages[d.Description] = nil
 		}
 	}
 
@@ -119,4 +125,26 @@ func (c *alarmCollector) shouldFilterAlarm(a *details) bool {
 	}
 
 	return c.filter.MatchString(a.Description) || c.filter.MatchString(a.Type)
+}
+
+func parseXML(b []byte, res *multiEngineResult) error {
+	if strings.Contains(string(b), "<multi-routing-engine-results") {
+		return xml.Unmarshal(b, res)
+	}
+
+	se := singleEngineResult{}
+
+	err := xml.Unmarshal(b, &se)
+	if err != nil {
+		return err
+	}
+
+	res.Information.RoutingEngines = []routingEngine{
+		{
+			Name:        "N/A",
+			AlarmInfo: se.Information,
+		},
+	}
+
+	return nil
 }

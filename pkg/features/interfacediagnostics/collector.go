30e8b043ee00d81d836c8adf559a69080f249625
Jakob Gastinger <jgastinger@anexia-it.com>
2023-07-26 11:51:24 +0200

    rename variable to be meaningful

--- pkg/features/interfacediagnostics/collector.go
+++ pkg/features/interfacediagnostics/collector.go
@@ -287,8 +287,8 @@ func interfaceMediaInfoFromRPCResult(interfaceMediaList *[]physicalInterface) ma
 	for _, i := range *interfaceMediaList {
 		if strings.HasPrefix(i.Name, "xe") || strings.HasPrefix(i.Name, "ge") || strings.HasPrefix(i.Name, "et") {
 			iface := i
-			afterThirdIndex := iface.Name[3:]
-			interfaceMediaDict[afterThirdIndex] = &iface
+			slotIndex := iface.Name[3:]
+			interfaceMediaDict[slotIndex] = &iface
 		}
 	}
 	return interfaceMediaDict

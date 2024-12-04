d27287833088eebae795d570f8849893d197e8e1
Vincent V. <vincent.vilenchik@proton.me>
2024-12-04 08:58:58 +0100

    Additional metrics for ISIS (#266)
    * added a feature for macsec
    
    * small refactoring of some functions
    
    * added support for fec mode to interfaces plugin
    
    * small edits
    
    * small edits
    
    * small edits + chamged the mtu metric name
    
    * removed unnecessary string clean-up
    
    * small fixes as per pull request
    
    * tiny editing
    
    * Net 818 (#3)
    
    * adding number of isis adanceis per interface. Saving progress
    
    * finished
    
    * saving
    
    * saving
    
    * fixed some issues from pull request
    
    * fixes from merge request
    
    * added small fixes from pull review
    
    * added four new metrics for isis
    
    * added additional ISIS metrics
    
    * sorted imports
    
    * applied fixes from MR
    
    * small edit
    
    * small re-org
    
    * small re-org
    
    ---------
    
    Co-authored-by: Vincent Vilenchik <vincent.vilenchik@deepl.com>
    
    ---------
    
    Co-authored-by: Vincent Vilenchik <vincent.vilenchik@deepl.com>

--- pkg/features/isis/rpc.go
+++ pkg/features/isis/rpc.go
@@ -2,6 +2,8 @@
 
 package isis
 
+import "encoding/xml"
+
 type result struct {
 	Information struct {
 		Adjacencies []adjacency `xml:"isis-adjacency"`
@@ -15,4 +17,34 @@ type adjacency struct {
 	AdjacencyState string `xml:"adjacency-state"`
 	Holdtime       int64  `xml:"holdtime"`
 	SNPA           string `xml:"snpa"`
+}
+
+type interfaces struct {
+	XMLName                  xml.Name `xml:"rpc-reply"`
+	Text                     string   `xml:",chardata"`
+	Junos                    string   `xml:"junos,attr"`
+	IsisInterfaceInformation struct {
+		Text          string `xml:",chardata"`
+		Xmlns         string `xml:"xmlns,attr"`
+		IsisInterface []struct {
+			InterfaceName      string  `xml:"interface-name"`
+			LSPInterval        float64 `xml:"lsp-interval"`
+			CSNPInterval       float64 `xml:"csnp-interval"`
+			HelloPadding       string  `xml:"hello-padding"`
+			MaxHelloSize       float64 `xml:"max-hello-size"`
+			InterfaceLevelData struct {
+				Level             string  `xml:"level"`
+				AdjacencyCount    float64 `xml:"adjacency-count"`
+				InterfacePriority float64 `xml:"interface-priority"`
+				Metric            float64 `xml:"metric"`
+				HelloTime         float64 `xml:"hello-time"`
+				HoldTime          float64 `xml:"holdtime"`
+				Passive           string  `xml:"passive"`
+			} `xml:"interface-level-data"`
+		} `xml:"isis-interface"`
+	} `xml:"isis-interface-information"`
+	Cli struct {
+		Text   string `xml:",chardata"`
+		Banner string `xml:"banner"`
+	} `xml:"cli"`
 }

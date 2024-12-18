bfdc2662e18b98ee733c58fb1dde6583621dbf61
takt <oliver.peter.herms@gmail.com>
2024-12-18 15:11:59 +0100

    Connection Manager: Disconnect and reconnect on failure (#273)
    Co-authored-by: Oliver Geiselhardt-Herms <ogh@deepl.com>

--- pkg/connector/connection_manager.go
+++ pkg/connector/connection_manager.go
@@ -3,13 +3,11 @@
 package connector
 
 import (
+	"fmt"
 	"net"
 	"strings"
 	"sync"
 	"time"
-
-	"github.com/pkg/errors"
-	"golang.org/x/crypto/ssh"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -51,11 +49,11 @@ func WithExpiredConnectionTimeout(d time.Duration) Option {
 // SSHConnectionManager manages SSH connections to different devices
 type SSHConnectionManager struct {
 	connections              map[string]*SSHConnection
+	connectionsMu            sync.RWMutex
 	reconnectInterval        time.Duration
 	keepAliveInterval        time.Duration
 	keepAliveTimeout         time.Duration
 	expiredConnectionTimeout time.Duration
-	locks                    map[string]*sync.Mutex
 }
 
 // NewConnectionManager creates a new connection manager
@@ -65,7 +63,6 @@ func NewConnectionManager(opts ...Option) *SSHConnectionManager {
 		reconnectInterval: 30 * time.Second,
 		keepAliveInterval: 10 * time.Second,
 		keepAliveTimeout:  15 * time.Second,
-		locks:             make(map[string]*sync.Mutex),
 	}
 
 	for _, opt := range opts {
@@ -75,80 +72,51 @@ func NewConnectionManager(opts ...Option) *SSHConnectionManager {
 	return m
 }
 
-func (m *SSHConnectionManager) lockForDevice(device *Device) *sync.Mutex {
-	if mu, exists := m.locks[device.Host]; exists {
-		return mu
-	}
-
-	mu := &sync.Mutex{}
-	m.locks[device.Host] = mu
-	return mu
-}
-
-// Connect connects to a device or returns an long living connection
-func (m *SSHConnectionManager) Connect(device *Device) (*SSHConnection, error) {
-	if connection, found := m.connections[device.Host]; found {
-		if connection.isConnected() {
-			return connection, nil
-		}
-	}
-
-	mu := m.lockForDevice(device)
-	mu.Lock()
-	defer mu.Unlock()
-
-	if connection, found := m.connections[device.Host]; found {
-		if connection.isConnected() {
-			return connection, nil
-		}
+// GetSSHConnection gets a cached SSHConnection or creates a fresh one, if necessary
+func (m *SSHConnectionManager) GetSSHConnection(device *Device) (*SSHConnection, error) {
+	connection := m.getExistingConnection(device)
+	if connection != nil {
+		log.Infof("Re-using existing connection with %s", device.Host)
+		return connection, nil
 	}
 
 	return m.connect(device)
 }
 
-func (m *SSHConnectionManager) connect(device *Device) (*SSHConnection, error) {
-	client, conn, err := m.connectToDevice(device)
-	if err != nil {
-		return nil, err
+func (m *SSHConnectionManager) getExistingConnection(device *Device) *SSHConnection {
+	m.connectionsMu.RLock()
+	defer m.connectionsMu.RUnlock()
+
+	if connection, found := m.connections[device.Host]; found {
+		if connection.IsConnected() {
+			return connection
+		}
 	}
 
-	c := &SSHConnection{
-		conn:   conn,
-		client: client,
-		device: device,
-		done:   make(chan struct{}),
+	return nil
+}
+
+func (m *SSHConnectionManager) connect(device *Device) (*SSHConnection, error) {
+	log.Infof("Creating SSH connection with %s", device.Host)
+	c := NewSSHConnection(device, m.keepAliveInterval, m.keepAliveTimeout)
+	err := c.Start(m.expiredConnectionTimeout)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get new SSH connection: %w", err)
 	}
-	go m.keepAlive(c)
+
+	m.connectionsMu.Lock()
+	defer m.connectionsMu.Unlock()
+
+	if existingCon, exists := m.connections[device.Host]; exists && existingCon.IsConnected() {
+		c.Stop(fmt.Errorf("connection conflict"))
+		return existingCon, nil
+	}
 
 	m.connections[device.Host] = c
-
 	return c, nil
 }
 
-func (m *SSHConnectionManager) connectToDevice(device *Device) (*ssh.Client, net.Conn, error) {
-	cfg := &ssh.ClientConfig{
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		Timeout:         timeoutInSeconds * time.Second,
-	}
-
-	device.Auth(cfg)
-
-	host := m.tcpAddressForHost(device.Host)
-
-	conn, err := net.DialTimeout("tcp", host, cfg.Timeout)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "could not open tcp connection")
-	}
-
-	c, chans, reqs, err := ssh.NewClientConn(conn, host, cfg)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "could not connect to device")
-	}
-
-	return ssh.NewClient(c, chans, reqs), conn, nil
-}
-
-func (m *SSHConnectionManager) tcpAddressForHost(host string) string {
+func tcpAddressForHost(host string) string {
 	colonCount := strings.Count(host, ":")
 
 	if colonCount == 0 {
@@ -158,13 +126,13 @@ func (m *SSHConnectionManager) tcpAddressForHost(host string) string {
 
 	h, p, err := net.SplitHostPort(host)
 	if err == nil {
-		return m.formatHost(h) + ":" + p
+		return formatHost(h) + ":" + p
 	}
 
-	return m.formatHost(host) + ":" + defaultPort
+	return formatHost(host) + ":" + defaultPort
 }
 
-func (m *SSHConnectionManager) formatHost(host string) string {
+func formatHost(host string) string {
 	ip := net.ParseIP(host)
 
 	if ip == nil || ip.To4() != nil {
@@ -175,47 +143,10 @@ func (m *SSHConnectionManager) formatHost(host string) string {
 	return "[" + host + "]"
 }
 
-func (m *SSHConnectionManager) keepAlive(connection *SSHConnection) {
-	for {
-		select {
-		case <-time.After(m.keepAliveInterval):
-			if time.Since(connection.lastUsed) > m.expiredConnectionTimeout {
-				connection.terminate()
-				return
-			}
-
-			log.Debugf("Sending keepalive for ")
-			connection.conn.SetDeadline(time.Now().Add(m.keepAliveTimeout))
-			_, _, err := connection.client.SendRequest("keepalive@golang.org", true, nil)
-			if err != nil {
-				log.Infof("Lost connection to %s (%v). Trying to reconnect...", connection.device, err)
-				connection.terminate()
-				m.reconnect(connection)
-			}
-		case <-connection.done:
-			return
-		}
-	}
-}
-
-func (m *SSHConnectionManager) reconnect(connection *SSHConnection) {
-	for {
-		client, conn, err := m.connectToDevice(connection.device)
-		if err == nil {
-			connection.client = client
-			connection.conn = conn
-			return
-		}
-
-		log.Infof("Reconnect to %s failed: %v", connection.device, err)
-		time.Sleep(m.reconnectInterval)
-	}
-}
-
-// Close closes all TCP connections and stop keep alives
-func (m *SSHConnectionManager) Close() error {
+// CloseAll closes all TCP connections and stops keep alives
+func (m *SSHConnectionManager) CloseAll() error {
 	for _, c := range m.connections {
-		c.close()
+		c.Stop(fmt.Errorf("end of world"))
 	}
 
 	return nil

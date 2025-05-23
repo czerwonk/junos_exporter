e095e90f0f97e8f3ff5e302bb3c6741f3a8c3190
Vincent V. <vincent.vilenchik@proton.me>
2025-05-23 13:07:35 +0200

    ISIS backup coverage metrics  (#286)
    * added local interface labels to related bgp metrics
    
    * made interface label to appear only when there is a value
    
    * added isis backup coverage
    
    * added the second part of the feature
    
    * edited the xml + small edits
    
    * removed unnecessary variable
    
    * started to work on tests
    
    * finished testing
    
    * small fix for coverage value + data driven testing
    
    * small fixes from review
    
    * small fixes
    
    * small fixes+ reading data from separate xml files properly
    
    * deleted old tests file
    
    * simplfied the check if backup path exists
    
    * removed one unneccessary if condition
    
    ---------
    
    Co-authored-by: Vincent Vilenchik <vincent.vilenchik@deepl.com>

--- pkg/features/isis/rpc.go
+++ pkg/features/isis/rpc.go
@@ -21,8 +21,6 @@ type adjacency struct {
 
 type interfaces struct {
 	XMLName                  xml.Name `xml:"rpc-reply"`
-	Text                     string   `xml:",chardata"`
-	Junos                    string   `xml:"junos,attr"`
 	IsisInterfaceInformation struct {
 		Text          string `xml:",chardata"`
 		Xmlns         string `xml:"xmlns,attr"`
@@ -47,4 +45,45 @@ type interfaces struct {
 		Text   string `xml:",chardata"`
 		Banner string `xml:"banner"`
 	} `xml:"cli"`
+}
+
+type backupCoverage struct {
+	XMLName                       xml.Name `xml:"rpc-reply"`
+	IsisBackupCoverageInformation struct {
+		IsisBackupCoverage struct {
+			Text                          string `xml:",chardata"`
+			IsisTopologyID                string `xml:"isis-topology-id"`
+			Level                         string `xml:"level"`
+			IsisNodeCoverage              string `xml:"isis-node-coverage"`
+			IsisRouteCoverageIpv4         string `xml:"isis-route-coverage-ipv4"`
+			IsisRouteCoverageIpv6         string `xml:"isis-route-coverage-ipv6"`
+			IsisRouteCoverageClns         string `xml:"isis-route-coverage-clns"`
+			IsisRouteCoverageIpv4Mpls     string `xml:"isis-route-coverage-ipv4-mpls"`
+			IsisRouteCoverageIpv6Mpls     string `xml:"isis-route-coverage-ipv6-mpls"`
+			IsisRouteCoverageIpv4MplsSspf string `xml:"isis-route-coverage-ipv4-mpls-sspf"`
+			IsisRouteCoverageIpv6MplsSspf string `xml:"isis-route-coverage-ipv6-mpls-sspf"`
+		} `xml:"isis-backup-coverage"`
+	} `xml:"isis-backup-coverage-information"`
+}
+
+type backupSPF struct {
+	XMLName            xml.Name `xml:"rpc-reply"`
+	IsisSpfInformation struct {
+		IsisSpf []struct {
+			IsisBackupSpfResult []struct {
+				NodeID                  string `xml:"node-id"`
+				NoCoverageReasonElement []struct {
+					IsisNextHopType  string `xml:"isis-next-hop-type"`
+					NoCoverageReason string `xml:"no-coverage-reason"`
+				} `xml:"no-coverage-reason-element"`
+				BackupNextHopElement struct {
+					InterfaceName            string  `xml:"interface-name"`
+					IsisNextHopType          string  `xml:"isis-next-hop-type"`
+					IsisBackupPrefixRefcount float64 `xml:"isis-backup-prefix-refcount"` // Converted to int
+					IsisNextHop              string  `xml:"isis-next-hop"`
+					SNPA                     string  `xml:"snpa"`
+				} `xml:"backup-next-hop-element"`
+			} `xml:"isis-backup-spf-result"`
+		} `xml:"isis-spf"`
+	} `xml:"isis-spf-information"`
 }

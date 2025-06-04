a0c6616be990ac159cdfd352b9e0651af26a9c98
Vincent V. <vincent.vilenchik@proton.me>
2025-06-04 07:02:09 +0200

    Fixed array out of index error in macsec feature (#288)

--- pkg/features/macsec/collector.go
+++ pkg/features/macsec/collector.go
@@ -5,10 +5,11 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/czerwonk/junos_exporter/pkg/collector"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/czerwonk/junos_exporter/pkg/collector"
 )
 
 const prefix string = "junos_macsec_"
@@ -113,28 +114,34 @@ func (c *macsecCollector) Collect(client collector.Client, ch chan<- prometheus.
 
 // collectForSessions collects metrics for the sessions
 func (c *macsecCollector) collectForInterfaces(sessions resultInt, ch chan<- prometheus.Metric, labelValues []string) {
-	for c, mici := range sessions.MacsecConnectionInformation.MacsecInterfaceCommonInformation {
+	for _, mici := range sessions.MacsecConnectionInformation.MacsecInterfaceCommonInformation {
 		labels := append(labelValues,
 			mici.InterfaceName,
 			mici.ConnectivityAssociationName)
-		pn, err := strconv.Atoi(sessions.MacsecConnectionInformation.OutboundSecureChannel[c].OutgoingPacketNumber)
-		if err != nil {
-			log.Errorf("unable to convert outgoing packets number: %q", sessions.MacsecConnectionInformation.OutboundSecureChannel[c].OutgoingPacketNumber)
+		if sessions.MacsecConnectionInformation.OutboundSecureChannel != nil {
+			pn, err := strconv.Atoi(sessions.MacsecConnectionInformation.OutboundSecureChannel.OutgoingPacketNumber)
+			if err != nil {
+				log.Errorf("unable to convert outgoing packets number: %q", sessions.MacsecConnectionInformation.OutboundSecureChannel.OutgoingPacketNumber)
+			}
+			ch <- prometheus.MustNewConstMetric(macsecTXPacketCountDesc, prometheus.CounterValue, float64(pn), labels...)
 		}
+
 		sci := convertYesNoToInt(strings.TrimRight(mici.IncludeSci, "\n"))
 		rp := convertOnOffToInt(strings.TrimRight(mici.ReplayProtect, "\n"))
 		kso, err := strconv.Atoi(mici.Offset)
 		if err != nil {
 			log.Errorf("unable to convert offset: %q", mici.Offset)
 		}
-		status := stateToFloat(sessions.MacsecConnectionInformation.OutboundSecureChannel[c].OutboundSecureAssociation.AssociationNumberStatus)
 		enc := convertOnOffToInt(strings.TrimRight(mici.Encryption, "\n"))
-		ch <- prometheus.MustNewConstMetric(macsecTXPacketCountDesc, prometheus.CounterValue, float64(pn), labels...)
+
 		ch <- prometheus.MustNewConstMetric(macsecIncludeSCIDesc, prometheus.GaugeValue, float64(sci), labels...)
 		ch <- prometheus.MustNewConstMetric(macsecReplayProtectDesc, prometheus.GaugeValue, float64(rp), labels...)
 		ch <- prometheus.MustNewConstMetric(macsecKeyServerOffsetDesc, prometheus.GaugeValue, float64(kso), labels...)
 		ch <- prometheus.MustNewConstMetric(macsecEncryptionDesc, prometheus.GaugeValue, float64(enc), labels...)
-		ch <- prometheus.MustNewConstMetric(macsecTXChannelStatusDesc, prometheus.GaugeValue, status, labels...)
+		if sessions.MacsecConnectionInformation.OutboundSecureChannel != nil {
+			status := stateToFloat(sessions.MacsecConnectionInformation.OutboundSecureChannel.OutboundSecureAssociation.AssociationNumberStatus)
+			ch <- prometheus.MustNewConstMetric(macsecTXChannelStatusDesc, prometheus.GaugeValue, status, labels...)
+		}
 	}
 }
 

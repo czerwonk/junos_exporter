b526a09290d78d2290e216588c7cdeeab25e31d8
Vincent Vilenchik <vincent.vilenchik@deepl.com>
2025-09-02 14:04:48 +0200

    updated tests for TCP

--- pkg/features/systemstatistics/rpc_test.go
+++ pkg/features/systemstatistics/rpc_test.go
@@ -160,7 +160,7 @@ func TestStatisticsIPv6Unmarshaling(t *testing.T) {
 	}
 	tests := []testCase{
 		{
-			name: "complete_ipv6_statistics",
+			name:     "complete_ipv6_statistics",
 			xmlInput: string(IPv6DataCase1),
 			expect: func(t *testing.T, got SystemStatistics) {
 				ip6 := got.Statistics.Ip6
@@ -243,7 +243,7 @@ func TestStatisticsUDPUnmarshaling(t *testing.T) {
 
 	tests := []testCase{
 		{
-			name: "complete_udp_statistics",
+			name:     "complete_udp_statistics",
 			xmlInput: string(UDPDataCase1),
 			expect: func(t *testing.T, got SystemStatistics) {
 				udp := got.Statistics.Udp
@@ -261,7 +261,6 @@ func TestStatisticsUDPUnmarshaling(t *testing.T) {
 			},
 		},
 	}
-
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
@@ -284,7 +283,7 @@ func TestStatisticsTCPUnmarshaling(t *testing.T) {
 
 	tests := []testCase{
 		{
-			name: "complete_tcp_statistics",
+			name:     "complete_tcp_statistics",
 			xmlInput: string(TCPDataCase1),
 			expect: func(t *testing.T, got SystemStatistics) {
 				tcp := got.Statistics.Tcp
@@ -294,30 +293,114 @@ func TestStatisticsTCPUnmarshaling(t *testing.T) {
 				assert.Equal(t, float64(10), tcp.SentDataPacketsRetransmitted)
 				assert.Equal(t, float64(2048), tcp.RetransmittedBytes)
 				assert.Equal(t, float64(50), tcp.SentAckOnlyPackets)
-				assert.Equal(t, float64(1100), tcp.PacketsReceived)
-				assert.Equal(t, float64(980), tcp.ReceivedAcks)
-				assert.Equal(t, float64(654321), tcp.AcksBytes)
-				assert.Equal(t, float64(1005), tcp.PacketsReceivedInSequence)
-				assert.Equal(t, float64(222333), tcp.Bytes)
-				assert.Equal(t, float64(40), tcp.ConnectionRequests)
-				assert.Equal(t, float64(39), tcp.ConnectionAccepts)
-				assert.Equal(t, float64(2), tcp.BadConnectionAttempts)
-				assert.Equal(t, float64(1), tcp.ListenQueueOverflows)
-				assert.Equal(t, float64(38), tcp.ConnectionsEstablished)
-				assert.Equal(t, float64(30), tcp.ConnectionsClosed)
-				assert.Equal(t, float64(3), tcp.Drops)
-				assert.Equal(t, float64(44), tcp.Attempts)
-				assert.Equal(t, float64(6), tcp.RetransmitTimeouts)
-				assert.Equal(t, float64(5), tcp.KeepaliveTimeouts)
-				assert.Equal(t, float64(15), tcp.KeepaliveProbesSent)
-				assert.Equal(t, float64(2), tcp.KeepaliveConnectionsDropped)
-				assert.Equal(t, float64(12), tcp.RstPackets)
-				assert.Equal(t, float64(7), tcp.SendPacketsDropped)
-				assert.Equal(t, float64(6), tcp.RcvPacketsDropped)
-				assert.Equal(t, float64(5), tcp.OutgoingSegmentsDropped)
-				assert.Equal(t, float64(20), tcp.OptionMaxsegmentLength)
-				assert.Equal(t, float64(10), tcp.OptionWindowLength)
-				assert.Equal(t, float64(12), tcp.OptionTimestampLength)
+				assert.Equal(t, float64(51), tcp.SentPacketsDelayed)
+				assert.Equal(t, float64(52), tcp.SentUrgOnlyPackets)
+				assert.Equal(t, float64(53), tcp.SentWindowProbePackets)
+				assert.Equal(t, float64(54), tcp.SentWindowUpdatePackets)
+				assert.Equal(t, float64(55), tcp.SentControlPackets)
+				assert.Equal(t, float64(56), tcp.PacketsReceived)
+				assert.Equal(t, float64(57), tcp.ReceivedAcks)
+				assert.Equal(t, float64(58), tcp.AcksBytes)
+				assert.Equal(t, float64(59), tcp.ReceivedDuplicateAcks)
+				assert.Equal(t, float64(60), tcp.ReceivedAcksForUnsentData)
+				assert.Equal(t, float64(61), tcp.PacketsReceivedInSequence)
+				assert.Equal(t, float64(62), tcp.InSequenceBytes)
+				assert.Equal(t, float64(63), tcp.ReceivedCompletelyDuplicatePacket)
+				assert.Equal(t, float64(64), tcp.DuplicateInBytes)
+				assert.Equal(t, float64(65), tcp.ReceivedOldDuplicatePackets)
+				assert.Equal(t, float64(66), tcp.ReceivedPacketsWithSomeDupliacteData)
+				assert.Equal(t, float64(67), tcp.SomeDuplicateInBytes)
+				assert.Equal(t, float64(68), tcp.ReceivedOutOfOrderPackets)
+				assert.Equal(t, float64(69), tcp.OutOfOrderInBytes)
+				assert.Equal(t, float64(70), tcp.ReceivedPacketsOfDataAfterWindow)
+				assert.Equal(t, float64(71), tcp.Bytes)
+				assert.Equal(t, float64(72), tcp.ReceivedWindowProbes)
+				assert.Equal(t, float64(73), tcp.ReceivedWindowUpdatePackets)
+				assert.Equal(t, float64(74), tcp.PacketsReceivedAfterClose)
+				assert.Equal(t, float64(75), tcp.ReceivedDiscardedForBadChecksum)
+				assert.Equal(t, float64(76), tcp.ReceivedDiscardedForBadHeaderOffset)
+				assert.Equal(t, float64(77), tcp.ReceivedDiscardedBecausePacketTooShort)
+				assert.Equal(t, float64(78), tcp.ConnectionRequests)
+				assert.Equal(t, float64(79), tcp.ConnectionAccepts)
+				assert.Equal(t, float64(80), tcp.BadConnectionAttempts)
+				assert.Equal(t, float64(81), tcp.ListenQueueOverflows)
+				assert.Equal(t, float64(82), tcp.BadRstWindow)
+				assert.Equal(t, float64(83), tcp.ConnectionsEstablished)
+				assert.Equal(t, float64(84), tcp.ConnectionsClosed)
+				assert.Equal(t, float64(85), tcp.Drops)
+				assert.Equal(t, float64(86), tcp.ConnectionsUpdatedRttOnClose)
+				assert.Equal(t, float64(87), tcp.ConnectionsUpdatedVarianceOnClose)
+				assert.Equal(t, float64(88), tcp.ConnectionsUpdatedSsthreshOnClose)
+				assert.Equal(t, float64(89), tcp.EmbryonicConnectionsDropped)
+				assert.Equal(t, float64(90), tcp.SegmentsUpdatedRtt)
+				assert.Equal(t, float64(91), tcp.Attempts)
+				assert.Equal(t, float64(92), tcp.RetransmitTimeouts)
+				assert.Equal(t, float64(93), tcp.ConnectionsDroppedByRetransmitTimeout)
+				assert.Equal(t, float64(94), tcp.PersistTimeouts)
+				assert.Equal(t, float64(95), tcp.ConnectionsDroppedByPersistTimeout)
+				assert.Equal(t, float64(96), tcp.KeepaliveTimeouts)
+				assert.Equal(t, float64(97), tcp.KeepaliveProbesSent)
+				assert.Equal(t, float64(98), tcp.KeepaliveConnectionsDropped)
+				assert.Equal(t, float64(99), tcp.AckHeaderPredictions)
+				assert.Equal(t, float64(100), tcp.DataPacketHeaderPredictions)
+				assert.Equal(t, float64(101), tcp.SyncacheEntriesAdded)
+				assert.Equal(t, float64(102), tcp.Retransmitted)
+				assert.Equal(t, float64(103), tcp.Dupsyn)
+				assert.Equal(t, float64(104), tcp.Dropped)
+				assert.Equal(t, float64(105), tcp.Completed)
+				assert.Equal(t, float64(106), tcp.BucketOverflow)
+				assert.Equal(t, float64(107), tcp.CacheOverflow)
+				assert.Equal(t, float64(108), tcp.Reset)
+				assert.Equal(t, float64(109), tcp.Stale)
+				assert.Equal(t, float64(110), tcp.Aborted)
+				assert.Equal(t, float64(111), tcp.Badack)
+				assert.Equal(t, float64(112), tcp.Unreach)
+				assert.Equal(t, float64(113), tcp.ZoneFailures)
+				assert.Equal(t, float64(114), tcp.CookiesSent)
+				assert.Equal(t, float64(115), tcp.CookiesReceived)
+				assert.Equal(t, float64(116), tcp.SackRecoveryEpisodes)
+				assert.Equal(t, float64(117), tcp.SegmentRetransmits)
+				assert.Equal(t, float64(118), tcp.ByteRetransmits)
+				assert.Equal(t, float64(119), tcp.SackOptionsReceived)
+				assert.Equal(t, float64(120), tcp.SackOpitionsSent)
+				assert.Equal(t, float64(121), tcp.SackScoreboardOverflow)
+				assert.Equal(t, float64(122), tcp.AcksSentInResponseButNotExactRsts)
+				assert.Equal(t, float64(123), tcp.AcksSentInResponseToSynsOnEstablishedConnections)
+				assert.Equal(t, float64(124), tcp.RcvPacketsDroppedDueToBadAddress)
+				assert.Equal(t, float64(125), tcp.OutOfSequenceSegmentDrops)
+				assert.Equal(t, float64(126), tcp.RstPackets)
+				assert.Equal(t, float64(127), tcp.IcmpPacketsIgnored)
+				assert.Equal(t, float64(128), tcp.SendPacketsDropped)
+				assert.Equal(t, float64(129), tcp.RcvPacketsDropped)
+				assert.Equal(t, float64(130), tcp.OutgoingSegmentsDropped)
+				assert.Equal(t, float64(131), tcp.ReceivedSynfinDropped)
+				assert.Equal(t, float64(132), tcp.ReceivedIpsecDropped)
+				assert.Equal(t, float64(133), tcp.ReceivedMacDropped)
+				assert.Equal(t, float64(134), tcp.ReceivedMinttlExceeded)
+				assert.Equal(t, float64(135), tcp.ListenstateBadflagsDropped)
+				assert.Equal(t, float64(136), tcp.FinwaitstateBadflagsDropped)
+				assert.Equal(t, float64(137), tcp.ReceivedDosAttack)
+				assert.Equal(t, float64(138), tcp.ReceivedBadSynack)
+				assert.Equal(t, float64(139), tcp.SyncacheZoneFull)
+				assert.Equal(t, float64(140), tcp.ReceivedRstFirewallfilter)
+				assert.Equal(t, float64(141), tcp.ReceivedNoackTimewait)
+				assert.Equal(t, float64(142), tcp.ReceivedNoTimewaitState)
+				assert.Equal(t, float64(143), tcp.ReceivedRstTimewaitState)
+				assert.Equal(t, float64(144), tcp.ReceivedTimewaitDrops)
+				assert.Equal(t, float64(145), tcp.ReceivedBadaddrTimewaitState)
+				assert.Equal(t, float64(146), tcp.ReceivedAckoffInSynSentrcvd)
+				assert.Equal(t, float64(147), tcp.ReceivedBadaddrFirewall)
+				assert.Equal(t, float64(148), tcp.ReceivedNosynSynSent)
+				assert.Equal(t, float64(149), tcp.ReceivedBadrstSynSent)
+				assert.Equal(t, float64(150), tcp.ReceivedBadrstListenState)
+				assert.Equal(t, float64(151), tcp.OptionMaxsegmentLength)
+				assert.Equal(t, float64(152), tcp.OptionWindowLength)
+				assert.Equal(t, float64(153), tcp.OptionTimestampLength)
+				assert.Equal(t, float64(154), tcp.OptionMd5Length)
+				assert.Equal(t, float64(155), tcp.OptionAuthLength)
+				assert.Equal(t, float64(156), tcp.OptionSackpermittedLength)
+				assert.Equal(t, float64(157), tcp.OptionSackLength)
+				assert.Equal(t, float64(158), tcp.OptionAuthoptionLength)
 				assert.Equal(t, "user@router>", got.Cli.Banner)
 			},
 		},
@@ -332,5 +415,3 @@ func TestStatisticsTCPUnmarshaling(t *testing.T) {
 		})
 	}
 }
-
-

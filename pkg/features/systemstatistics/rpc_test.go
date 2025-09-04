cb715ad38bf716cdfc349a1d198c49eb5640f015
Vincent Vilenchik <vincent.vilenchik@deepl.com>
2025-09-04 09:36:08 +0200

    refactored tests for TCP

--- pkg/features/systemstatistics/rpc_test.go
+++ pkg/features/systemstatistics/rpc_test.go
@@ -2,7 +2,6 @@ package systemstatistics
 
 import (
 	"encoding/xml"
-	"io"
 	"os"
 	"testing"
 
@@ -233,145 +232,155 @@ func TestStatisticsUDPUnmarshaling(t *testing.T) {
 }
 
 func TestStatisticsTCPUnmarshaling(t *testing.T) {
-	TCPXMLDataCase1, _ := os.Open("testsFiles/TCP/TCPTestDataCase1.xml")
-	TCPDataCase1, _ := io.ReadAll(TCPXMLDataCase1)
 	type testCase struct {
-		name     string
-		xmlInput string
-		expect   func(t *testing.T, got SystemStatistics)
+		name    string
+		xmlFile string
+		expect  SystemStatistics
 	}
 
-	tests := []testCase{
+	testsTCP := []testCase{
 		{
-			name:     "complete_tcp_statistics",
-			xmlInput: string(TCPDataCase1),
-			expect: func(t *testing.T, got SystemStatistics) {
-				tcp := got.Statistics.Tcp
-				assert.Equal(t, float64(1000), tcp.PacketsSent)
-				assert.Equal(t, float64(900), tcp.SentDataPackets)
-				assert.Equal(t, float64(123456), tcp.DataPacketsBytes)
-				assert.Equal(t, float64(10), tcp.SentDataPacketsRetransmitted)
-				assert.Equal(t, float64(2048), tcp.RetransmittedBytes)
-				assert.Equal(t, float64(50), tcp.SentAckOnlyPackets)
-				assert.Equal(t, float64(51), tcp.SentPacketsDelayed)
-				assert.Equal(t, float64(52), tcp.SentUrgOnlyPackets)
-				assert.Equal(t, float64(53), tcp.SentWindowProbePackets)
-				assert.Equal(t, float64(54), tcp.SentWindowUpdatePackets)
-				assert.Equal(t, float64(55), tcp.SentControlPackets)
-				assert.Equal(t, float64(56), tcp.PacketsReceived)
-				assert.Equal(t, float64(57), tcp.ReceivedAcks)
-				assert.Equal(t, float64(58), tcp.AcksBytes)
-				assert.Equal(t, float64(59), tcp.ReceivedDuplicateAcks)
-				assert.Equal(t, float64(60), tcp.ReceivedAcksForUnsentData)
-				assert.Equal(t, float64(61), tcp.PacketsReceivedInSequence)
-				assert.Equal(t, float64(62), tcp.InSequenceBytes)
-				assert.Equal(t, float64(63), tcp.ReceivedCompletelyDuplicatePacket)
-				assert.Equal(t, float64(64), tcp.DuplicateInBytes)
-				assert.Equal(t, float64(65), tcp.ReceivedOldDuplicatePackets)
-				assert.Equal(t, float64(66), tcp.ReceivedPacketsWithSomeDupliacteData)
-				assert.Equal(t, float64(67), tcp.SomeDuplicateInBytes)
-				assert.Equal(t, float64(68), tcp.ReceivedOutOfOrderPackets)
-				assert.Equal(t, float64(69), tcp.OutOfOrderInBytes)
-				assert.Equal(t, float64(70), tcp.ReceivedPacketsOfDataAfterWindow)
-				assert.Equal(t, float64(71), tcp.Bytes)
-				assert.Equal(t, float64(72), tcp.ReceivedWindowProbes)
-				assert.Equal(t, float64(73), tcp.ReceivedWindowUpdatePackets)
-				assert.Equal(t, float64(74), tcp.PacketsReceivedAfterClose)
-				assert.Equal(t, float64(75), tcp.ReceivedDiscardedForBadChecksum)
-				assert.Equal(t, float64(76), tcp.ReceivedDiscardedForBadHeaderOffset)
-				assert.Equal(t, float64(77), tcp.ReceivedDiscardedBecausePacketTooShort)
-				assert.Equal(t, float64(78), tcp.ConnectionRequests)
-				assert.Equal(t, float64(79), tcp.ConnectionAccepts)
-				assert.Equal(t, float64(80), tcp.BadConnectionAttempts)
-				assert.Equal(t, float64(81), tcp.ListenQueueOverflows)
-				assert.Equal(t, float64(82), tcp.BadRstWindow)
-				assert.Equal(t, float64(83), tcp.ConnectionsEstablished)
-				assert.Equal(t, float64(84), tcp.ConnectionsClosed)
-				assert.Equal(t, float64(85), tcp.Drops)
-				assert.Equal(t, float64(86), tcp.ConnectionsUpdatedRttOnClose)
-				assert.Equal(t, float64(87), tcp.ConnectionsUpdatedVarianceOnClose)
-				assert.Equal(t, float64(88), tcp.ConnectionsUpdatedSsthreshOnClose)
-				assert.Equal(t, float64(89), tcp.EmbryonicConnectionsDropped)
-				assert.Equal(t, float64(90), tcp.SegmentsUpdatedRtt)
-				assert.Equal(t, float64(91), tcp.Attempts)
-				assert.Equal(t, float64(92), tcp.RetransmitTimeouts)
-				assert.Equal(t, float64(93), tcp.ConnectionsDroppedByRetransmitTimeout)
-				assert.Equal(t, float64(94), tcp.PersistTimeouts)
-				assert.Equal(t, float64(95), tcp.ConnectionsDroppedByPersistTimeout)
-				assert.Equal(t, float64(96), tcp.KeepaliveTimeouts)
-				assert.Equal(t, float64(97), tcp.KeepaliveProbesSent)
-				assert.Equal(t, float64(98), tcp.KeepaliveConnectionsDropped)
-				assert.Equal(t, float64(99), tcp.AckHeaderPredictions)
-				assert.Equal(t, float64(100), tcp.DataPacketHeaderPredictions)
-				assert.Equal(t, float64(101), tcp.SyncacheEntriesAdded)
-				assert.Equal(t, float64(102), tcp.Retransmitted)
-				assert.Equal(t, float64(103), tcp.Dupsyn)
-				assert.Equal(t, float64(104), tcp.Dropped)
-				assert.Equal(t, float64(105), tcp.Completed)
-				assert.Equal(t, float64(106), tcp.BucketOverflow)
-				assert.Equal(t, float64(107), tcp.CacheOverflow)
-				assert.Equal(t, float64(108), tcp.Reset)
-				assert.Equal(t, float64(109), tcp.Stale)
-				assert.Equal(t, float64(110), tcp.Aborted)
-				assert.Equal(t, float64(111), tcp.Badack)
-				assert.Equal(t, float64(112), tcp.Unreach)
-				assert.Equal(t, float64(113), tcp.ZoneFailures)
-				assert.Equal(t, float64(114), tcp.CookiesSent)
-				assert.Equal(t, float64(115), tcp.CookiesReceived)
-				assert.Equal(t, float64(116), tcp.SackRecoveryEpisodes)
-				assert.Equal(t, float64(117), tcp.SegmentRetransmits)
-				assert.Equal(t, float64(118), tcp.ByteRetransmits)
-				assert.Equal(t, float64(119), tcp.SackOptionsReceived)
-				assert.Equal(t, float64(120), tcp.SackOpitionsSent)
-				assert.Equal(t, float64(121), tcp.SackScoreboardOverflow)
-				assert.Equal(t, float64(122), tcp.AcksSentInResponseButNotExactRsts)
-				assert.Equal(t, float64(123), tcp.AcksSentInResponseToSynsOnEstablishedConnections)
-				assert.Equal(t, float64(124), tcp.RcvPacketsDroppedDueToBadAddress)
-				assert.Equal(t, float64(125), tcp.OutOfSequenceSegmentDrops)
-				assert.Equal(t, float64(126), tcp.RstPackets)
-				assert.Equal(t, float64(127), tcp.IcmpPacketsIgnored)
-				assert.Equal(t, float64(128), tcp.SendPacketsDropped)
-				assert.Equal(t, float64(129), tcp.RcvPacketsDropped)
-				assert.Equal(t, float64(130), tcp.OutgoingSegmentsDropped)
-				assert.Equal(t, float64(131), tcp.ReceivedSynfinDropped)
-				assert.Equal(t, float64(132), tcp.ReceivedIpsecDropped)
-				assert.Equal(t, float64(133), tcp.ReceivedMacDropped)
-				assert.Equal(t, float64(134), tcp.ReceivedMinttlExceeded)
-				assert.Equal(t, float64(135), tcp.ListenstateBadflagsDropped)
-				assert.Equal(t, float64(136), tcp.FinwaitstateBadflagsDropped)
-				assert.Equal(t, float64(137), tcp.ReceivedDosAttack)
-				assert.Equal(t, float64(138), tcp.ReceivedBadSynack)
-				assert.Equal(t, float64(139), tcp.SyncacheZoneFull)
-				assert.Equal(t, float64(140), tcp.ReceivedRstFirewallfilter)
-				assert.Equal(t, float64(141), tcp.ReceivedNoackTimewait)
-				assert.Equal(t, float64(142), tcp.ReceivedNoTimewaitState)
-				assert.Equal(t, float64(143), tcp.ReceivedRstTimewaitState)
-				assert.Equal(t, float64(144), tcp.ReceivedTimewaitDrops)
-				assert.Equal(t, float64(145), tcp.ReceivedBadaddrTimewaitState)
-				assert.Equal(t, float64(146), tcp.ReceivedAckoffInSynSentrcvd)
-				assert.Equal(t, float64(147), tcp.ReceivedBadaddrFirewall)
-				assert.Equal(t, float64(148), tcp.ReceivedNosynSynSent)
-				assert.Equal(t, float64(149), tcp.ReceivedBadrstSynSent)
-				assert.Equal(t, float64(150), tcp.ReceivedBadrstListenState)
-				assert.Equal(t, float64(151), tcp.OptionMaxsegmentLength)
-				assert.Equal(t, float64(152), tcp.OptionWindowLength)
-				assert.Equal(t, float64(153), tcp.OptionTimestampLength)
-				assert.Equal(t, float64(154), tcp.OptionMd5Length)
-				assert.Equal(t, float64(155), tcp.OptionAuthLength)
-				assert.Equal(t, float64(156), tcp.OptionSackpermittedLength)
-				assert.Equal(t, float64(157), tcp.OptionSackLength)
-				assert.Equal(t, float64(158), tcp.OptionAuthoptionLength)
-				//assert.Equal(t, "user@router>", got.Cli.Banner)
+			name:    "complete_tcp_statistics",
+			xmlFile: "testsFiles/TCP/TCPTestDataCase1.xml",
+			expect: SystemStatistics{
+				Statistics: Statistics{
+					Tcp: TCP{
+						PacketsSent:                            4000,
+						SentDataPackets:                        4001,
+						DataPacketsBytes:                       4002,
+						SentDataPacketsRetransmitted:           4003,
+						RetransmittedBytes:                     4004,
+						SentDataUnnecessaryRetransmitted:       4005,
+						SentResendsByMtuDiscovery:              4006,
+						SentAckOnlyPackets:                     4007,
+						SentPacketsDelayed:                     4008,
+						SentUrgOnlyPackets:                     4009,
+						SentWindowProbePackets:                 4010,
+						SentWindowUpdatePackets:                4011,
+						SentControlPackets:                     4012,
+						PacketsReceived:                        4013,
+						ReceivedAcks:                           4014,
+						AcksBytes:                              4015,
+						ReceivedDuplicateAcks:                  4016,
+						ReceivedAcksForUnsentData:              4017,
+						PacketsReceivedInSequence:              4018,
+						InSequenceBytes:                        4019,
+						ReceivedCompletelyDuplicatePacket:      4020,
+						DuplicateInBytes:                       4021,
+						ReceivedOldDuplicatePackets:            4022,
+						ReceivedPacketsWithSomeDupliacteData:   4023,
+						SomeDuplicateInBytes:                   4024,
+						ReceivedOutOfOrderPackets:              4025,
+						OutOfOrderInBytes:                      4026,
+						ReceivedPacketsOfDataAfterWindow:       4027,
+						Bytes:                                  4028,
+						ReceivedWindowProbes:                   4029,
+						ReceivedWindowUpdatePackets:            4030,
+						PacketsReceivedAfterClose:              4031,
+						ReceivedDiscardedForBadChecksum:        4032,
+						ReceivedDiscardedForBadHeaderOffset:    4033,
+						ReceivedDiscardedBecausePacketTooShort: 4034,
+						ConnectionRequests:                     4035,
+						ConnectionAccepts:                      4036,
+						BadConnectionAttempts:                  4037,
+						ListenQueueOverflows:                   4038,
+						BadRstWindow:                           4039,
+						ConnectionsEstablished:                 4040,
+						ConnectionsClosed:                      4041,
+						Drops:                                  4042,
+						ConnectionsUpdatedRttOnClose:           4043,
+						ConnectionsUpdatedVarianceOnClose:      4044,
+						ConnectionsUpdatedSsthreshOnClose:      4045,
+						EmbryonicConnectionsDropped:            4046,
+						SegmentsUpdatedRtt:                     4047,
+						Attempts:                               4048,
+						RetransmitTimeouts:                     4049,
+						ConnectionsDroppedByRetransmitTimeout:  4050,
+						PersistTimeouts:                        4051,
+						ConnectionsDroppedByPersistTimeout:     4052,
+						KeepaliveTimeouts:                      4053,
+						KeepaliveProbesSent:                    4054,
+						KeepaliveConnectionsDropped:            4055,
+						AckHeaderPredictions:                   4056,
+						DataPacketHeaderPredictions:            4057,
+						SyncacheEntriesAdded:                   4058,
+						Retransmitted:                          4059,
+						Dupsyn:                                 4060,
+						Dropped:                                4061,
+						Completed:                              4062,
+						BucketOverflow:                         4063,
+						CacheOverflow:                          4064,
+						Reset:                                  4065,
+						Stale:                                  4066,
+						Aborted:                                4067,
+						Badack:                                 4068,
+						Unreach:                                4069,
+						ZoneFailures:                           4070,
+						CookiesSent:                            4071,
+						CookiesReceived:                        4072,
+						SackRecoveryEpisodes:                   4073,
+						SegmentRetransmits:                     4074,
+						ByteRetransmits:                        4075,
+						SackOptionsReceived:                    4076,
+						SackOpitionsSent:                       4077,
+						SackScoreboardOverflow:                 4078,
+						AcksSentInResponseButNotExactRsts:      4079,
+						AcksSentInResponseToSynsOnEstablishedConnections: 4080,
+						RcvPacketsDroppedDueToBadAddress:                 4081,
+						OutOfSequenceSegmentDrops:                        4082,
+						RstPackets:                                       4083,
+						IcmpPacketsIgnored:                               4084,
+						SendPacketsDropped:                               4085,
+						RcvPacketsDropped:                                4086,
+						OutgoingSegmentsDropped:                          4087,
+						ReceivedSynfinDropped:                            4088,
+						ReceivedIpsecDropped:                             4089,
+						ReceivedMacDropped:                               4090,
+						ReceivedMinttlExceeded:                           4091,
+						ListenstateBadflagsDropped:                       4092,
+						FinwaitstateBadflagsDropped:                      4093,
+						ReceivedDosAttack:                                4094,
+						ReceivedBadSynack:                                4095,
+						SyncacheZoneFull:                                 4096,
+						ReceivedRstFirewallfilter:                        4097,
+						ReceivedNoackTimewait:                            4098,
+						ReceivedNoTimewaitState:                          4099,
+						ReceivedRstTimewaitState:                         4100,
+						ReceivedTimewaitDrops:                            4101,
+						ReceivedBadaddrTimewaitState:                     4102,
+						ReceivedAckoffInSynSentrcvd:                      4103,
+						ReceivedBadaddrFirewall:                          4104,
+						ReceivedNosynSynSent:                             4105,
+						ReceivedBadrstSynSent:                            4106,
+						ReceivedBadrstListenState:                        4107,
+						OptionMaxsegmentLength:                           4108,
+						OptionWindowLength:                               4109,
+						OptionTimestampLength:                            4110,
+						OptionMd5Length:                                  4111,
+						OptionAuthLength:                                 4112,
+						OptionSackpermittedLength:                        4113,
+						OptionSackLength:                                 4114,
+						OptionAuthoptionLength:                           4115,
+					},
+				},
 			},
 		},
 	}
 
-	for _, tc := range tests {
+	for _, tc := range testsTCP {
 		t.Run(tc.name, func(t *testing.T) {
-			var got SystemStatistics
-			err := xml.Unmarshal([]byte(tc.xmlInput), &got)
+			fc, err := os.ReadFile(tc.xmlFile)
+			if err != nil {
+
+			}
+			var result SystemStatistics
+			err = xml.Unmarshal(fc, &result)
+			if err != nil {
+
+			}
+			result.Statistics.Tcp.Text = ""
+			assert.Equal(t, tc.expect.Statistics.Tcp, result.Statistics.Tcp, tc.name)
 			assert.NoError(t, err, "unmarshal should not return error")
-			tc.expect(t, got)
 		})
 	}
 }

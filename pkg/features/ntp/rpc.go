7ad45d8e48bb4d392a30f095fc97325b54cac655
gerhweber <gerhard.weber@de.logicalis.com>
2025-12-18 15:20:30 +0100

    Update rpc.go for ntp
    Please see comment for collector.go

--- pkg/features/ntp/rpc.go
+++ pkg/features/ntp/rpc.go
@@ -1,41 +1,25 @@
-// In rpc.go NUR folgendes belassen:
 package ntp
 
-import (
-	"encoding/xml"
-	"regexp"
-        "strings"
-)
+import "encoding/xml"
 
 type rpcReply struct {
 	XMLName xml.Name `xml:"rpc-reply"`
-	Output  struct {
+
+	Output struct {
 		Text string `xml:",chardata"`
 	} `xml:"output"`
-}
 
-type parseResult struct {
-	AssocID      string
-	Stratum      float64
-	RefID        string
-	Offset       float64
-	SysJitter    float64
-	ClkJitter    float64
-	RootDelay    float64
-	Leap         string
-	Precision    float64
-	PollInterval float64
-}
-
-func parseNTPOutput(output string) map[string]string {
-	re := regexp.MustCompile(`(\w+)=("[^"]+"|\S+)`)
-	matches := re.FindAllStringSubmatch(output, -1)
-
-	metrics := make(map[string]string)
-	for _, m := range matches {
-		key := m[1]
-                value := strings.Trim(m[2], "\", ")
-		metrics[key] = value
-	}
-	return metrics
+	NtpStatus struct {
+		AssocID   string `xml:"associd"`
+		Status    string `xml:"status"`
+		Leap      string `xml:"leap"`
+		Stratum   string `xml:"stratum"`
+		Precision string `xml:"precision"`
+		Rootdelay string `xml:"rootdelay"`
+		Refid     string `xml:"refid"`
+		Offset    string `xml:"offset"`
+		SysJitter string `xml:"sys-jitter"`
+		ClkJitter string `xml:"clk-jitter"`
+		Tc        string `xml:"tc"`
+	} `xml:"ntp-status"`
 }

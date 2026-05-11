c17348847e5f765d17681f212caa600a9b869234
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2026-05-11 09:04:50 +0200

    chore: bump version

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const version string = "0.15.3"
+const version string = "0.15.4"
 
 var (
 	showVersion                 = flag.Bool("version", false, "Print version information.")

5330b4346668f2538bf945d2297f1e4332dfec4d
mcmufffin <34554507+mcmufffin@users.noreply.github.com>
2026-09-16 10:14:58 +0000

    Update rpc.go
    include hardware model struct

--- pkg/features/firewall/rpc.go
+++ pkg/features/firewall/rpc.go
@@ -3,25 +3,31 @@
 package firewall
 
 type result struct {
-	Information struct {
-		Filters []filter `xml:"filter-information"`
-	} `xml:"firewall-information"`
+        Information struct {
+                Filters []filter `xml:"filter-information"`
+        } `xml:"firewall-information"`
+}
+
+type systemInformation struct {
+        SystemInformation struct {
+                HardwareModel string `xml:"hardware-model"`
+        } `xml:"system-information"`
 }
 
 type filter struct {
-	Name     string          `xml:"filter-name"`
-	Counters []filterCounter `xml:"counter"`
-	Policers []filterPolicer `xml:"policer"`
+        Name     string          `xml:"filter-name"`
+        Counters []filterCounter `xml:"counter"`
+        Policers []filterPolicer `xml:"policer"`
 }
 
 type filterCounter struct {
-	Name    string `xml:"counter-name"`
-	Packets int64  `xml:"packet-count"`
-	Bytes   int64  `xml:"byte-count"`
+        Name    string `xml:"counter-name"`
+        Packets int64  `xml:"packet-count"`
+        Bytes   int64  `xml:"byte-count"`
 }
 
 type filterPolicer struct {
-	Name    string `xml:"policer-name"`
-	Packets int64  `xml:"packet-count"`
-	Bytes   int64  `xml:"byte-count"`
+        Name    string `xml:"policer-name"`
+        Packets int64  `xml:"packet-count"`
+        Bytes   int64  `xml:"byte-count"`
 }
